135f5565d972a9e9aaec4af9058dff8bc7e0b760
xonoxs <135383475+xonoxs@users.noreply.github.com>
2023-06-06 22:45:13 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -17,8 +17,8 @@
 import Vue from 'vue';
 import App from './App.vue';
 // You can change this import to `import router from './starterRouter'` to quickly start development from a blank layout.
-import router from './router';
-import NowUiKit from './plugins/now-ui-kit';
+import router from './src/router';
+import NowUiKit from './src/plugins/now-ui-kit';
 
 Vue.config.productionTip = false;
 
