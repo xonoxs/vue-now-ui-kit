8a5d57f2cc91ad17614e9ca70d5a5d2ff73be8e2
xonoxs <135383475+xonoxs@users.noreply.github.com>
2023-06-06 23:59:59 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   pages: {
     index: {
       // entry for the page
-      entry: 'src/index/main.js',
+      entry: 'src/main.js',
       // the source template
       template: 'public/index.html',
       // output as dist/index.html
