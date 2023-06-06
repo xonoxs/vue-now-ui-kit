092c55ec21f0f6eaee87ee7116af1685c979c0eb
xonoxs <135383475+xonoxs@users.noreply.github.com>
2023-06-06 23:44:07 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -6,30 +6,6 @@ module.exports = {
       title: 'Welcome to my vue generator project',
       chunks: ['chunk-vendors', 'chunk-common', 'index']
     },
-    'bad': {
-      entry: './src/error-instance.ts',
-      template: 'public/bad.html',
-      title: 'Error page',
-      chunks: ['chunk-vendors', 'chunk-common', 'index']
-    },
-    /* Disabled - Only one time
-    'googleVerify': {
-      entry: './src/error-instance.ts',
-      template: 'public/somelink.html',
-      title: 'Error page',
-      chunks: ['chunk-vendors', 'chunk-common', 'index']
-    },
-    */
-
   },
-  devServer: {
-    'port': 3000
-  },
-  css: {
-    sourceMap: false
-  },
-  pwa: {
-    name: 'My App',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
+}
  
