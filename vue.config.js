caf2d1a401f988abb0effd019fdece5e0439a84a
xonoxs <135383475+xonoxs@users.noreply.github.com>
2023-06-06 23:37:14 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,35 @@
-var path = require('path');
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production'
-    ? '/xonoxs.github.io/'
-    : './public/',
-	configureWebpack : {
-		resolve: {
-	        modules : [
-		        path.resolve("./src"),
-	            path.resolve("./node_modules")
-	        ]
-	    },
-	}
-}
+  pages: {
+    'index': {
+      entry: './src/main.ts',
+      template: 'public/index.html',
+      title: 'Welcome to my vue generator project',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    'bad': {
+      entry: './src/error-instance.ts',
+      template: 'public/bad.html',
+      title: 'Error page',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    /* Disabled - Only one time
+    'googleVerify': {
+      entry: './src/error-instance.ts',
+      template: 'public/somelink.html',
+      title: 'Error page',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    */
+
+  },
+  devServer: {
+    'port': 3000
+  },
+  css: {
+    sourceMap: false
+  },
+  pwa: {
+    name: 'My App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+ 
