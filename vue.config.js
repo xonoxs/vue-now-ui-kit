67e09aae30962a0a99d116632ef7404d7023c520
xonoxs <135383475+xonoxs@users.noreply.github.com>
2023-06-06 22:55:06 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/xonoxs.github.io/'
-    : './public/'
+    : './public/',
+  css: {
+    // Enable CSS source maps.
+    sourceMap: process.env.NODE_ENV !== 'production' ? "/xonoxs.github.io/" : "./src/assets/",
+  }
 }
